db506db35d6e8a78d6b471509abf5ef621e6560f
Dave Chesser <dave@deaches.net>
2025-12-01 17:36:07 -0500

    Day 1: Secret Entrance, part 1

---
+++ day01.lua
@@ -0,0 +1,65 @@
+#!/usr/bin/env lua
+-- Advent of Code 2025: Lua Edition
+-- Day 1: Secret Entrance
+-- Solution by “Deaches” (@deaches.bsky.social)
+--
+-- This is free and unencumbered software released into the public domain.
+--
+-- Anyone is free to copy, modify, publish, use, compile, sell, or
+-- distribute this software, either in source code form or as a compiled
+-- binary, for any purpose, commercial or non-commercial, and by any means.
+--
+-- In jurisdictions that recognize copyright laws, the author or authors
+-- of this software dedicate any and all copyright interest in the
+-- software to the public domain. We make this dedication for the benefit
+-- of the public at large and to the detriment of our heirs and
+-- successors. We intend this dedication to be an overt act of
+-- relinquishment in perpetuity of all present and future rights to this
+-- software under copyright law.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+-- IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+-- OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE.
+--
+-- For more information, please refer to <https://unlicense.org>
+
+local pointing = 50
+local zeroes   = 0
+
+-- Returns a positive number if you rotate it right,
+-- negative number if you rotate it left.
+function readrotation (str)
+   local direction, magnitude = str:match("([LR])(%d+)")
+
+   if direction == nil or magnitude == nil then
+      error("Unable to read direction \"" .. str .. "\"")
+   else
+      magnitude = math.abs(tonumber(magnitude))
+   end
+
+   if direction == "L" then
+      return -magnitude
+   elseif direction == "R" then
+      return magnitude
+   else
+      error("Unable to determine how to rotate " .. distance .. " units")
+   end
+end
+
+--  --  --
+
+for line in io.lines() do
+   local rotation = readrotation(line)
+
+   pointing = (pointing + rotation) % 100
+
+   if pointing == 0 then
+      zeroes = zeroes + 1
+   end
+end
+
+print(zeroes)
